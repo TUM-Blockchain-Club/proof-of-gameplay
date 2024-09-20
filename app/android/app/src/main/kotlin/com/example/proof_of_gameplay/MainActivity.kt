e9186f12eb0fc2c90f1f6d7dd39c3b047160a13d
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-09-21 03:55:16 +0800

    Add template for camera plugin

---
+++ app/android/app/src/main/kotlin/com/example/proof_of_gameplay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proof_of_gameplay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ app/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const ProofOfGameplayApp());
+}
+
+/// CameraApp is the Main Application.
+class ProofOfGameplayApp extends StatefulWidget {
+  /// Default Constructor
+  const ProofOfGameplayApp({super.key});
+
+  @override
+  State<ProofOfGameplayApp> createState() => _ProofOfGameplayAppState();
+}
+
+class _ProofOfGameplayAppState extends State<ProofOfGameplayApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: CameraPreview(controller),
+    );
+  }
+}
