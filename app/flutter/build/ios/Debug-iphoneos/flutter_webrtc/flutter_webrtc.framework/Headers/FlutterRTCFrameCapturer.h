ab741cb846a2d0582488e12cf1a657b055d05f58
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-10-07 17:25:15 +0200

    Move flutter app into flutter subdirectory

---
+++ app/flutter/build/ios/Debug-iphoneos/flutter_webrtc/flutter_webrtc.framework/Headers/FlutterRTCFrameCapturer.h
@@ -0,0 +1,15 @@
+#import <WebRTC/WebRTC.h>
+
+#if TARGET_OS_IPHONE
+#import <Flutter/Flutter.h>
+#elif TARGET_OS_OSX
+#import <FlutterMacOS/FlutterMacOS.h>
+#endif
+
+@interface FlutterRTCFrameCapturer : NSObject <RTCVideoRenderer>
+
+- (instancetype)initWithTrack:(RTCVideoTrack*)track
+                       toPath:(NSString*)path
+                       result:(FlutterResult)result;
+
+@end
