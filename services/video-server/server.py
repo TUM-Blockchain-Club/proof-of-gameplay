95932c6430b8020c7ae0f71440ed4363ef171c01
Luis Bahners <m3r8@web.de>
2024-10-09 05:29:23 +0200

    deleted database and added to gitignore

--- services/video-server/server.py
+++ services/video-server/server.py
@@ -202,7 +202,7 @@ def verify():
         return {"Error": "video and input data didnt match"}
     print("matching successful!!!",file=sys.stderr)
     print("simulating game...",file=sys.stderr)
-    print(tinputData)
+    #print(tinputData)
     score = simulate(tinputData)
     print("player: " + str(playerID) + " got a proven score of: " + str(score),file=sys.stderr)
     print("Signing score...", file=sys.stderr)
